697bc513b8c31f0f07fdc54f1d74c4e84f6e9a74
Naira <naira.drh@gmail.com>
2018-10-21 13:57:25 +0100

    Lab2 V2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const { Pool } = require ('pg');
+const pool = new Pool ({
+  connectionString: 'postgres://thcoizirihgxji:662ba6ceb6ef5b404a0a82c889e7693aec0d8cb963968e5b4240d25ce0fc525a@ec2-23-21-166-148.compute-1.amazonaws.com:5432/dd2e9rnufbb68c',
+  ssl: true
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/playlist', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query ('SELECT * FROM playlist');
+    const results = { 'results': (result) ? result.rows : null };
+    res.render('playlist', results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("ERROR " + err);
+  }
 });
 
 module.exports = router;
